880f21d87c6de24c4ee239c3099a3e668e743780
Luis Antonio Montoya Morales <devlumniom@gmail.com>
2024-05-06 21:07:26 -0600

    Update divfreq.v
    Ultima practica

--- monocicloChido - copia/divfreq.v
+++ monocicloChido - copia/divfreq.v
@@ -1,34 +1,38 @@
-module divfreq (
+/*
+	Grupo:			5CV3
+	Proyecto:		Monociclo
+	Archivo:			div_freq.v
+	Equipo:			Equipo 2
+	Integrantes:	Alcaraz Cuevas Victor Hugo
+						Montoya Morales Luis Antonio
+						Navarrete Becerril Sharon Anette
+						Quintana Romero Roberto
+						Urdaneta Villalobos Paul Alejandro
 
+	Descripcion:	Divisor de frecuencias a 1Hz
+*/
+module div_freq(
 	input clk_i,
 	input rst_ni,
-	
 	output reg clk_o
 );
 
-	//SECCIÓN DE DECLARACIÓN DE SEÑALES
-	reg	[31:0]cuenta;
+	reg [31:0] cuenta_r;
 
-	always @(posedge clk_i, negedge rst_ni)
-		begin
-			if(!rst_ni)
-			begin
-				clk_o<=1'b1;
-				cuenta<=32'b0;
+	always @(posedge clk_i, negedge rst_ni) begin
+		if(!rst_ni) begin
+			clk_o <= 1'b1;
+			cuenta_r <= 32'b0;
+		end
+		else begin
+			if(cuenta_r == 32'd50_000_000) begin
+					cuenta_r <= 32'd0;
+					clk_o <= ~clk_o;
 			end
-			else
-			begin
-				if(cuenta==32'd50_000_000)
-				begin
-					cuenta<=32'b0;
-					clk_o<=~clk_o;
-				end
-				else
-				begin
-					cuenta<=cuenta+1'b1;
-				end
+			else begin
+				cuenta_r <= cuenta_r + 1'b1;
 			end
 		end
-
-
-endmodule
+	end
+	
+endmodule 
